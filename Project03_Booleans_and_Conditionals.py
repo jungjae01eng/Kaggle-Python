11bcf0d51429b615ca048f4adf0baa6cb641e0b5
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-06-10 23:59:29 -0400

    Update Project03_Booleans_and_Conditionals.py

--- Project03_Booleans_and_Conditionals.py
+++ Project03_Booleans_and_Conditionals.py
@@ -1,6 +1,7 @@
 # @author Jungjae Lee
 # Independent Study
 # Created on June 8, 2022
+# Last Updated on June 10, 2022
 # Sources: Functions and Getting Help by Colin Morris on the Kaggle
 # --------------------------------------------------------------------------------
 
@@ -31,7 +32,7 @@ print("Can a 45-year-old run for president?", can_run_for_president(45))
 
 
 def is_odd(n):
-    return(n%2) == 1
+    return(n % 2) == 1
 
 
 print("Is 100 odd", is_odd(100))
@@ -49,3 +50,69 @@ def can_run_for_president02(age, is_natural_born_citizen):
 print(can_run_for_president02(19, True))
 print(can_run_for_president02(55, False))
 print(can_run_for_president02(55, True))
+
+
+# --------------------------------------------------------------------------------
+# ------------------------------ Operator Precedence -----------------------------
+# Lists are from the highest precedence(most binding) to the lowest precedence(least binding)
+# (expressions), [expressions], {key: value}, {expressions}     => Binding or parenthesized expression, list display,
+#                                                                   dictionary display, set display
+# x[index], x[index:index], x(arguments), x.attribute           => Subscription, Slicing, call, attribute reference
+# await x                                                       => Await expression
+# **                                                            => Exponentiation
+# +x, -x, ~x                                                    => positive, negative, bitwise NOT
+# *, @, /, //, %                        => Multiplication, matrix multiplication, division, floor division, remainder
+# +, -                                                          => Addition and subtraction
+# <<, >>                                                        => Shifts
+# &                                                             => Bitwise AND
+# ^                                                             => Bitwise XOR
+# |                                                             => Bitwise OR
+# in, not in, is, is not, <, <=, >, >=, !=, ==                  => Comparisons (membership tests and identity test)
+# not x                                                         => Boolean NOT
+# and                                                           => Boolean AND
+# or                                                            => Boolean OR
+# if - else                                                     => Conditional expression
+# lambda                                                        => Lambda expression
+# :=                                                            => Assignment expression
+
+
+# --------------------------------------------------------------------------------
+# --------------------------------- Conditionals ---------------------------------
+def inspect(x2):
+    if x2 == 0:
+        print(x2, "is zero")
+    elif x2 > 0:
+        print(x2, "is positive")
+    elif x2 < 0:
+        print(x2, "is negative")
+    else:
+        print(x2, "is unlike anything I've ever seen...")
+
+
+inspect(0)
+inspect(-15)
+
+
+def f(x3):
+    if x3 > 0:
+        print("Only printed when x is positive; x =", x3)
+        print("Also only printed when x is positive; x =", x3)
+    print("Always printed, regardless of x's value; x =", x3)
+
+
+f(1)
+f(0)
+
+
+# --------------------------------------------------------------------------------
+# ------------------------------ Boolean Conversion ------------------------------
+print(bool(1))          # All numbers are treated as true, except 0
+print(bool(0))
+print(bool("asf"))      # All strings are treated as true, except the empty string ""
+print(bool(""))
+
+
+if 0:
+    print(0)
+elif "spam":
+    print("spam")
